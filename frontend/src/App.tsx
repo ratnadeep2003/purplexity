d94201949aa404176d6b289818a7fdb6be618b69
ratnadeep2003 <ratnadeepabitkar@gmail.com>
2026-09-22 21:33:42 +0530

    wire up routing for auth and dashboard

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,15 @@
-import { BrowserRouter, Routes, Route } from "react-router";
+import { BrowserRouter, Route, Routes } from "react-router";
 import Auth from "./pages/Auth";
 import Dashboard from "./pages/Dashboard";
+
 export function App() {
   return (
     <BrowserRouter>
       <Routes>
-          <Route path='/auth' element={<Auth />}/>
-          <Route path='/' element={<Dashboard/>}/>
+        <Route path="/auth" element={<Auth />} />
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/c/:conversationId" element={<Dashboard />} />
+        <Route path="*" element={<Dashboard />} />
       </Routes>
     </BrowserRouter>
   );
